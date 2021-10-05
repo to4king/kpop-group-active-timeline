de9840217c970aadc04a7e49ae208940ac7d31c1
toshi <to4king@gmail.com>
2021-10-06 03:07:38 +0900

    add sample data

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
           [
             { type: "string", label: "Task ID" },
             { type: "string", label: "Task Name" },
+            { type: "string", label: "Resource" },
             { type: "date", label: "Start Date" },
             { type: "date", label: "End Date" },
             { type: "number", label: "Duration" },
@@ -20,52 +21,47 @@ function App() {
             { type: "string", label: "Dependencies" },
           ],
           [
-            "Research",
-            "Find sources",
-            new Date(2015, 0, 1),
-            new Date(2015, 0, 5),
+            "bts",
+            "BTS",
+            "members: 5",
+            new Date(2013, 5, 13),
+            new Date(),
             null,
-            100,
+            null,
             null,
           ],
           [
-            "Write",
-            "Write paper",
+            "txt",
+            "TOMORROW X TOGETHER",
+            "members: 4",
+            new Date(2019, 2, 4),
+            new Date(),
             null,
-            new Date(2015, 0, 9),
-            3 * 24 * 60 * 60 * 1000,
-            25,
-            "Research,Outline",
+            null,
+            null,
           ],
           [
-            "Cite",
-            "Create bibliography",
+            "2am",
+            "2AM",
+            "members: 4",
+            new Date(2008, 6, 11),
+            new Date(2014, 11, 31),
             null,
-            new Date(2015, 0, 7),
-            1 * 24 * 60 * 60 * 1000,
-            20,
-            "Research",
-          ],
-          [
-            "Complete",
-            "Hand in paper",
             null,
-            new Date(2015, 0, 10),
-            1 * 24 * 60 * 60 * 1000,
-            0,
-            "Cite,Write",
-          ],
-          [
-            "Outline",
-            "Outline paper",
             null,
-            new Date(2015, 0, 6),
-            1 * 24 * 60 * 60 * 1000,
-            100,
-            "Research",
           ],
         ]}
-        rootProps={{ "data-testid": "1" }}
+        options={{
+          gantt: {
+            criticalPathEnabled: false, // Critical path arrows will be the same as other arrows.
+            arrow: {
+              angle: 100,
+              width: 5,
+              color: "green",
+              radius: 0,
+            },
+          },
+        }}
       />
     </div>
   );
