30d67dc62bf9c94a04177e6af09979cbf04dc4ac
toshi <to4king@gmail.com>
2021-10-06 01:36:14 +0900

    add example chart

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,73 @@ import React from "react";
 import Chart from "react-google-charts";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Chart
+        width={"100%"}
+        height={"400px"}
+        chartType="Gantt"
+        loader={<div>Loading Chart</div>}
+        data={[
+          [
+            { type: "string", label: "Task ID" },
+            { type: "string", label: "Task Name" },
+            { type: "date", label: "Start Date" },
+            { type: "date", label: "End Date" },
+            { type: "number", label: "Duration" },
+            { type: "number", label: "Percent Complete" },
+            { type: "string", label: "Dependencies" },
+          ],
+          [
+            "Research",
+            "Find sources",
+            new Date(2015, 0, 1),
+            new Date(2015, 0, 5),
+            null,
+            100,
+            null,
+          ],
+          [
+            "Write",
+            "Write paper",
+            null,
+            new Date(2015, 0, 9),
+            3 * 24 * 60 * 60 * 1000,
+            25,
+            "Research,Outline",
+          ],
+          [
+            "Cite",
+            "Create bibliography",
+            null,
+            new Date(2015, 0, 7),
+            1 * 24 * 60 * 60 * 1000,
+            20,
+            "Research",
+          ],
+          [
+            "Complete",
+            "Hand in paper",
+            null,
+            new Date(2015, 0, 10),
+            1 * 24 * 60 * 60 * 1000,
+            0,
+            "Cite,Write",
+          ],
+          [
+            "Outline",
+            "Outline paper",
+            null,
+            new Date(2015, 0, 6),
+            1 * 24 * 60 * 60 * 1000,
+            100,
+            "Research",
+          ],
+        ]}
+        rootProps={{ "data-testid": "1" }}
+      />
+    </div>
+  );
 }
 
 export default App;
